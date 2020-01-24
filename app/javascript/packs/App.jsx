d30ad1124a07c7fd83bd33aa9a746addaf912af4
astrocket <leonardo.lee1993@gmail.com>
2020-01-24 18:26:54 +0900

    404 페이지 작업

--- app/javascript/packs/App.jsx
+++ app/javascript/packs/App.jsx
@@ -9,7 +9,10 @@ function App() {
     <div className="leading-normal tracking-normal text-gray-900">
       <Navigation/>
       <div className="pb-14 bg-right bg-cover">
-        {routeResult || <h1>404 on React SPA</h1>}
+        {routeResult || <div className="w-screen flex items-center justify-center flex-col" style={{ height: '50vh' }}>
+          <span className="text-5xl">😲</span><br/>
+          <span className="text-3xl">없는 페이지 입니다.</span>
+        </div>}
       </div>
     </div>
   );
