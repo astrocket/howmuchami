99b65f06ec5c5e887ad0cc3681389e61a675f5c4
astrocket <leonardo.lee1993@gmail.com>
2020-01-28 00:17:49 +0900

    fix => about text style

--- app/javascript/packs/routes.js
+++ app/javascript/packs/routes.js
@@ -3,7 +3,7 @@ import HomeIndex from "./pages/home/Index.jsx";
 
 const routes = {
     "/": () => <HomeIndex />,
-    "/about": () => <div className="container mx-auto h-16 flex items-center justify-center">Live in the future and build what seems interesting.</div>
+    "/about": () => <p className="m-5 h-16 text-center flex items-center justify-center">Live in the future and build what seems interesting.</p>
 };
 
 export default routes;
