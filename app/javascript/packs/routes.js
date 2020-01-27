cc549ffc0e1b8b3c2de2c6af79e893fd77e32fe6
astrocket <leonardo.lee1993@gmail.com>
2020-01-28 08:50:04 +0900

    add => facebook link

--- app/javascript/packs/routes.js
+++ app/javascript/packs/routes.js
@@ -3,7 +3,10 @@ import HomeIndex from "./pages/home/Index.jsx";
 
 const routes = {
     "/": () => <HomeIndex />,
-    "/about": () => <p className="m-5 h-16 text-center flex items-center justify-center">Live in the future and build what seems interesting.</p>
+    "/about": () => <p className="m-5 h-16 text-center flex flex-col items-center justify-center">
+        Live in the future and build what seems interesting.<br />
+        <a href="https://www.facebook.com/astrocket" className="mt-3 text-blue-500 hover:underline" target="_blank">facebook.com/astrocket</a>
+    </p>
 };
 
 export default routes;
