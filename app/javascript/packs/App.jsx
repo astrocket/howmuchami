46ef2d41c4190897ceeee4c0d506abc897cbc3de
astrocket <leonardo.lee1993@gmail.com>
2020-02-06 23:06:57 +0900

    add => connect hourRate Hook to Context Provider

--- app/javascript/packs/App.jsx
+++ app/javascript/packs/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import {useRoutes, A} from "hookrouter";
+import {useRoutes} from "hookrouter";
 import routes from "./routes";
-import HourRateContext from "./contexts/hourRate.context";
+import hourRateContext from "./contexts/hourRate.context";
+import useHourRate  from "./hooks/hourRate.hook";
 import Navigation from "./components/Navigation.jsx";
 import Footer from "./components/Footer.jsx";
 import LikeButton from "./components/LikeButton";
 
 function App() {
   const routeResult = useRoutes(routes);
+  const hourRate = useHourRate();
+
   return (
-    <HourRateContext.Provider>
+    <hourRateContext.Provider value={hourRate}>
       <div className="leading-normal tracking-normal text-gray-900">
         <Navigation/>
         <div className="pb-14 bg-right bg-cover">
@@ -21,7 +24,7 @@ function App() {
         <Footer/>
         <LikeButton />
       </div>
-    </HourRateContext.Provider>
+    </hourRateContext.Provider>
   );
 }
 
