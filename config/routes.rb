f259ce0cc8488e01f1076c27b2721fa58f2408f0
astrocket <leonardo.lee1993@gmail.com>
2020-05-01 17:58:46 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -26,4 +26,7 @@ Rails.application.routes.draw do
   %w( 404 422 500 ).each do |code|
     get code, :to => "errors#show", :code => code
   end
+
+  get 'health_check', to: 'home#health_check'
+
 end
